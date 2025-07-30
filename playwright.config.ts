b5d6e64406da55e674baf0227d049def0b2706e4
Ivan Doroshenko <idorosenko24@gmail.com>
2025-07-30 15:47:41 +0300

    write test.img

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,37 +6,13 @@ export default defineConfig({
   timeout: 30 * 1000, // Загальний таймаут для тестів (30 секунд)
 
   // Глобальні налаштування для всіх проектів
-  use: {
-    browserName: 'chromium',
-    headless: false,
-    viewport: { width: 1920, height: 1080 }, // Стандартний розмір вікна
-    ignoreHTTPSErrors: true,
-    bypassCSP: true,
-    launchOptions: {
-      args: [
-        '--ignore-certificate-errors',
-        '--incognito',
-        '--disable-dev-shm-usage',
-        '--disable-gpu'
-      ],
-    },
-    trace: 'on-first-retry', // Записувати трейс для першої спроби повтору
-    video: 'on-first-retry', // Записувати відео для першої спроби повтору
-    screenshot: 'on', // Зберігати скріншоти
-    baseURL: 'http://localhost:3000', // Базовий URL для тестів
-  },
+
 
   // Настройки для запуску з Allure
   reporter: [
-    ['allure-playwright'],
-    ['html']
+    ['allure-playwright']
   ],
 
   // Проект для Chrome
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
-  ]
+
 });
